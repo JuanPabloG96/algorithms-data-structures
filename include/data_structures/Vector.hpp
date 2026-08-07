1d4eae0b1878d4869435dc5e14062f068177dad4
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2026-08-07 02:03:30 -0600

    add: vector class, implementing rule of five

--- include/data_structures/Vector.hpp
+++ include/data_structures/Vector.hpp
@@ -1,48 +1,115 @@
 #pragma once
 
+#include <algorithm>
 #include <cstddef>
+#include <new>
 
 template <typename T> class Vector
 {
 private:
-    T m_data;
-    size_t m_size;
-    size_t m_capacity;
+    T* data;
+    size_t capacity;
+    size_t size;
+
+    void clear()
+    {
+        for (size_t i = 0; i < size; i++)
+        {
+            data[i].~T();
+        }
+        ::operator delete(data);
+        reset();
+    }
+
+    void reset()
+    {
+        data = nullptr;
+        size = 0;
+        capacity = 0;
+    }
 
 public:
-    Vector() {}
+    Vector() : capacity(2), size(0)
+    {
+        data = static_cast<T*>(::operator new(capacity * sizeof(T)));
+    }
 
-    Vector(size_t initial_capacity) : m_capacity(initial_capacity) {}
+    explicit Vector(size_t initial_capacity)
+        : capacity(initial_capacity), size(initial_capacity)
+    {
+        data = static_cast<T*>(::operator new(capacity * sizeof(T)));
+
+        for (size_t i = 0; i < size; i++)
+        {
+            new (&data[i]) T();
+        }
+    }
+
+    Vector(const Vector& other) : capacity(other.capacity), size(other.size)
+    {
+        data = static_cast<T*>(::operator new(capacity * sizeof(T)));
+
+        for (size_t i = 0; i < other.size; i++)
+        {
+            new (&data[i]) T(other[i]);
+        }
+    }
+
+    Vector& operator=(const Vector& other)
+    {
+        if (this == &other)
+            return *this;
+
+        clear();
+
+        this->capacity = other.capacity;
+        this->size = other.size;
+
+        data = static_cast<T*>(::operator new(capacity * sizeof(T)));
+
+        for (size_t i = 0; i < other.size; i++)
+        {
+            new (&data[i]) T(other[i]);
+        }
+
+        return *this;
+    }
+
+    Vector(Vector&& other) noexcept
+        : capacity(other.capacity), size(other.size), data(other.data)
+    {
+        other.reset();
+    }
+
+    Vector& operator=(Vector&& other) noexcept
+    {
+        if (this == &other)
+            return *this;
+
+        clear();
+
+        data = other.data;
+        capacity = other.capacity;
+        size = other.size;
+
+        other.reset();
+
+        return *this;
+    }
 
     ~Vector() { clear(); }
 
-    Vector(const Vector& other) {}
+    T& operator[](size_t index) { return data[index]; }
 
-    Vector& operator=(const Vector& other) {}
+    const T& operator[](size_t index) const { return data[index]; }
 
-    Vector(Vector&& other) {}
+    void pushBack(T&& element)
+    {
+        new (&data[size]) T(std::move(element));
+        size++;
+    }
 
-    Vector& operator=(Vector&& other) {}
+    size_t getCapacity() const { return capacity; }
 
-    size_t size() const { return this->m_size; }
-
-    size_t capacity() const { return this->capacity(); }
-
-    bool isEmpty() const { return m_size == 0; }
-
-    void reserve(size_t new_capacity) {}
-
-    void push_back(const T& valor) {}
-
-    void push_back(T&& valor) {}
-
-    void pop_back() {}
-
-    void clear() {}
-
-    T& operator[](size_t indice) {}
-
-    const T& operator[](size_t indice) const {}
-
-    T& at(size_t indice) {}
+    size_t getSize() const { return size; }
 };
